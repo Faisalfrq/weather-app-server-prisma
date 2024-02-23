a8ffcbcfba461bb50ff2d6ab54b7ec7c6d745bc7
QasimMalikk <iamqasimk@gmail.com>
2024-02-23 14:54:31 +0500

    /

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 //index.js
 const express =require('express') 
-// import { AppRoutes } from './src/controllers';
+import { AppRoutes } from './src/controllers';
 const app = express();
 const PORT = process.env.PORT || 3002;
 
-// app.use(AppRoutes())
+app.use(AppRoutes())
 app.get("/", (req:any, res:any) => {
   res.send("Hello, world! PieTECH DEV");
 });
