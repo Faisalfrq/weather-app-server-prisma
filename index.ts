fe597096c75e27a24b7693e0f233f4b52254b5e2
QasimMalikk <iamqasimk@gmail.com>
2024-02-23 16:34:55 +0500

    againn

--- index.ts
+++ index.ts
@@ -5,9 +5,6 @@ const app = express();
 const PORT = process.env.PORT || 3002;
 
 app.use(routes())
-app.get("/", (req:any, res:any) => {
-  res.send("Hello, world! PieTECH DEV");
-});
 
 // Start the server
 app.listen(PORT, () => {
